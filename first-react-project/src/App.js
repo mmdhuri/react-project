c97096f8919e18e432d1311502c3c88ba5f885f5
Manish Dhuri <manishdhuri2000@gmail.com>
2024-07-22 19:32:14 +0530

    Basic Counter Application

--- first-react-project/src/App.js
+++ first-react-project/src/App.js
@@ -1,10 +1,21 @@
-import Contact from "./Contact"
-
+//import Contact from "./Contact"
+import {useState} from 'react'
 function App() {
+ let [counter,setCounter] = useState(1);
+  const addValue = () =>{
+    counter = counter + 1;
+    setCounter(counter);
+  }
+
+  const decreaseValue = () =>{
+    counter = counter - 1;
+    setCounter(counter);
+  }
   return (
     <div>
-      <h1>Hello Worlds</h1>
-      <Contact/>
+      <h1>Counter App {counter}</h1>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={decreaseValue}>Decrease Value</button>
     </div>
   );
 }
